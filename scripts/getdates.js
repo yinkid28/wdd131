68e91265e83bc1c29d56cc7cffbd40f09630455b
Adeyinka ADedayo <adeyinkaadedayo1024@mail.com>
2025-05-05 12:49:22 +0100

    week 01 assignment completed

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Set current year in footer
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Set last modified date
+document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
---
+++ week01/scripts/test.js
@@ -0,0 +1,4 @@
+let myNumber = "500"; // oops, this is still a string
+typeof myNumber;
+myNumber = 500; // much better — now this is a number
+typeof myNumber;
